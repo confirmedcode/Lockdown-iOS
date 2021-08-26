06999ea6fbb6b60c130ad6a585f8964de4c942e1
shalloran <sean@halloran.io>
2021-08-26 17:30:43 -0400

    BLOCKLIST: Additional Braze SDK

--- FirewallUtilities.swift
+++ FirewallUtilities.swift
@@ -228,7 +228,7 @@ func setupFirewallDefaultBlockLists() {
         ipRanges: [:])
     
     let marketingScriptsII = LockdownGroup.init(
-        version: 28,
+        version: 29,
         internalID: "marketing_beta_scripts",
         name: NSLocalizedString("Marketing Trackers II", comment: "The title of a list of trackers"),
         iconURL: "marketing_icon",
