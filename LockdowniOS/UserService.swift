518f44ce116fd64e6627dd577670f224f3b29eac
Pavel Vilbik <Pavel.Vilbik@joinappex.com>
2023-07-05 15:27:27 +0300

    refactoring: simplify update user subscription

--- LockdowniOS/UserService.swift
+++ LockdowniOS/UserService.swift
@@ -22,54 +22,47 @@ final class BaseUserService: UserService {
     var user = LockdownUser()
     
     func updateUserSubscription(completion: @escaping (Subscription?) -> Void) {
+        firstly {
+            signIn()
+        }.then { _ in
+            try Client.activeSubscriptions()
+        }.done { subscriptions in
+            DDLogInfo("active-subs: \(subscriptions)")
+            NotificationCenter.default.post(name: AccountUI.accountStateDidChange, object: self)
+            self.user.updateSubscription(to: subscriptions.first)
+            completion(subscriptions.first)
+        }.catch { error in
+            DDLogError("Error reloading subscription: \(error.localizedDescription)")
+            if let apiError = error as? ApiError {
+                DDLogError("Error loading plan: API error code - \(apiError.code)")
+            } else {
+                DDLogError("Error loading plan: Non-API Error - \(error.localizedDescription)")
+            }
+            self.user.updateSubscription(to: nil)
+            // TODO: to change later when there will be data on the server
+            completion(nil)
+        }
+    }
+    
+    private func signIn() -> Promise<()> {
         if let apiCredentials = getAPICredentials(), getAPICredentialsConfirmed() == true {
             DDLogInfo("plan status: have confirmed API credentials, using them")
-            firstly {
+            return firstly {
                 try Client.signInWithEmail(email: apiCredentials.email, password: apiCredentials.password)
             }
             .then { (signin: SignIn) -> Promise<SubscriptionEvent> in
                 DDLogInfo("plan status: signin result: \(signin)")
                 return try Client.subscriptionEvent()
             }
-            .then { (result: SubscriptionEvent) -> Promise<[Subscription]> in
+            .then { result in
                 DDLogInfo("plan status: subscriptionevent result: \(result)")
-                return try Client.activeSubscriptions()
-            }.done { subscriptions in
-                DDLogInfo("active-subs: \(subscriptions)")
-                NotificationCenter.default.post(name: AccountUI.accountStateDidChange, object: self)
-                
-                self.user.updateSubscription(to: subscriptions.first)
-                completion(subscriptions.first)
-            }
-            .catch { error in
-                if let apiError = error as? ApiError {
-                    DDLogError("Error loading plan: API error code - \(apiError.code)")
-                } else {
-                    DDLogError("Error loading plan: Non-API Error - \(error.localizedDescription)")
-                }
-                self.user.updateSubscription(to: nil)
-                completion(nil)
+                return Promise { seal in seal.fulfill(()) }
             }
         } else {
-            firstly {
+            return firstly {
                 try Client.signIn()
             }.then { _ in
-                try Client.activeSubscriptions()
-            }.done { subscriptions in
-                DDLogInfo("active-subs: \(subscriptions)")
-                NotificationCenter.default.post(name: AccountUI.accountStateDidChange, object: self)
-                self.user.updateSubscription(to: subscriptions.first)
-                completion(subscriptions.first)
-            }.catch { error in
-                DDLogError("Error reloading subscription: \(error.localizedDescription)")
-                if let apiError = error as? ApiError {
-                    DDLogError("Error loading plan: API error code - \(apiError.code)")
-                } else {
-                    DDLogError("Error loading plan: Non-API Error - \(error.localizedDescription)")
-                }
-                self.user.updateSubscription(to: nil)
-                // TODO: to change later when there will be data on the server
-                completion(nil)
+                Promise { seal in seal.fulfill(()) }
             }
         }
     }
