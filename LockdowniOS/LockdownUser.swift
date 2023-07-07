a90e7f4ad9ebbd7178884758c00f2b5e5f1a517e
Pavel Vilbik <Pavel.Vilbik@joinappex.com>
2023-07-07 12:47:36 +0300

    add caching subscription to Lockdown user
    # Conflicts:
    #	LockdowniOS.xcodeproj/project.pbxproj

--- LockdowniOS/LockdownUser.swift
+++ LockdowniOS/LockdownUser.swift
@@ -6,10 +6,38 @@
 //  Copyright © 2023 Confirmed Inc. All rights reserved.
 //
 
+import Foundation
+
 final class LockdownUser {
     private(set) var currentSubscription: Subscription?
     
+    @CodableUserDefaults(key: "cahedUsedSubscription")
+    private var cachedUserSubscription: Subscription?
+    
+    private let graceTimeInterval = TimeInterval(7 * 24 * 60 * 60) // 7 days
+    
     func updateSubscription(to newSubscription: Subscription?) {
         currentSubscription = newSubscription
+        updateCachedSubscription(to: newSubscription)
+    }
+    
+    private func updateCachedSubscription(to newSubscription: Subscription?) {
+        guard let newSubscription else {
+            return
+        }
+        
+        cachedUserSubscription = newSubscription
+    }
+    
+    func cachedSubscription() -> Subscription? {
+        guard let subscription = cachedUserSubscription else {
+            return nil
+        }
+        
+        guard subscription.expirationDate.addingTimeInterval(graceTimeInterval) > Date() else {
+            cachedUserSubscription = nil
+            return nil
+        }
+        return subscription
     }
 }
